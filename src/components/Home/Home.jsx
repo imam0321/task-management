ddccc778bebf7a162bed5eb68d64300697f1e444
imam0321 <imam.hossain0321@gmail.com>
2024-12-23 22:37:32 +0600

    Project setup

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Header } from '../Header/Header'
+import { Footer } from '../Footer/Footer'
+
+export const Home = () => {
+  return (
+    <>
+      <Header/>
+      <Footer/>
+    </>
+  )
+}
